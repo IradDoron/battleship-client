eead3dbc37acedd2593b5b510f6bc4b902f91f7c
Irad Doron <irad.doron@gmail.com>
2022-09-05 14:41:40 +0300

    add /

--- src/constants/SOCKET.js
+++ src/constants/SOCKET.js
@@ -2,7 +2,7 @@ import io from 'socket.io-client';
 
 const url = {
 	development: 'http://localhost:5001',
-	production: 'https://battleship-server.herokuapp.com',
+	production: 'https://battleship-server.herokuapp.com/',
 };
 
 const SOCKET = io(url.production);
