bb9b97da335946329189d87052714a085981179a
Irad Doron <irad.doron@gmail.com>
2022-09-05 14:26:55 +0300

    change url for production

--- src/constants/SOCKET.js
+++ src/constants/SOCKET.js
@@ -1,5 +1,10 @@
 import io from 'socket.io-client';
 
-const SOCKET = io('http://localhost:5001');
+const url = {
+	development: 'http://localhost:5001',
+	production: 'https://battleship-server.herokuapp.com',
+};
+
+const SOCKET = io(url.production);
 
 export default SOCKET;
